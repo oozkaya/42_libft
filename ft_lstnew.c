646ad9509db75e682bef05eae552f2cc2c76a204
Onur OZKAYA <yuonur@gmail.com>
2017-12-07 13:29:18 +0000

    ft_lstnew.c edited online with Bitbucket

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -16,7 +16,7 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 {
 	t_list	*list;
 
-	if (!(list = (t_list*)malloc(sizeof(*list))))
+	if (!(list = (t_list*)malloc(sizeof(*list) * content_size)))
 		return (NULL);
 	if (!content)
 	{
@@ -25,7 +25,7 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 	}
 	else
 	{
-		if (!(list->content = (void*)malloc(sizeof(*content))))
+		if (!(list->content = (void*)malloc(sizeof(content) * content_size)))
 			return (NULL);
 		ft_memcpy(list->content, content, content_size);
 		list->content_size = content_size;
